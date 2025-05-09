97b91d2d89607c371d363126bc72898e2b4136c3
KC <nachosktc@gmail.com>
2025-05-09 13:32:35 -0600

    add purchase, buyer, and item classes

--- src/js/model.js
+++ src/js/model.js
@@ -1,3 +1,49 @@
+/**
+ * @param item instance of item
+ * @param buyer instance of buyer
+ * @param raw original row
+ */
+class Purchase {
+  constructor({ quantity, item, buyer, date, raw }) {
+    this.quantity = quantity;
+    this.item = item;
+    this.buyer = buyer;
+    this.date = new Date(date);
+    this.raw = raw;
+  }
+}
+
+class Buyer {
+  constructor(name) {
+    this.name = name;
+    this.purchases = [];
+  }
+
+  add(purchase) {
+    this.purchases.push(purchase);
+  }
+
+  totalQuantity() {
+    return this.purchases.reduce((sum, p) => sum + p.quantity, 0);
+  }
+}
+
+class Item {
+  constructor(name) {
+    this.name = name;
+    this.purchases = [];
+  }
+
+  add(purchase) {
+    this.purchases.push(purchase);
+  }
+
+  totalQuantity() {
+    return this.purchases.reduce((sum, p) => sum + p.quantity, 0);
+  }
+}
+
+
 function analyzeRow(row, selectedItem) {
   const description = row.description || '';
   const doneAt = row.doneAt || '';
