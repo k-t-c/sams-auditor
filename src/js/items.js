1c4d8ddf6081e2843da02219b2a663eb2f2e654d
KC <nachosktc@gmail.com>
2025-05-17 23:57:32 -0600

    add item descriptions

--- src/js/items.js
+++ src/js/items.js
@@ -3,14 +3,16 @@ const ITEM_DEFINITIONS = {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 300000, // 5 minutes
+      timeInterval: 300000,
+      timeDescription: "5 minutes",
     },
   },
   "Bandage": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 300000, // 5 minutes
+      timeInterval: 300000,
+      timeDescription: "5 minutes",
     },
   },
   "Airbag Set": {
@@ -18,20 +20,23 @@ const ITEM_DEFINITIONS = {
       perSingleTransaction: 0,
       perTimeInterval: 0,
       timeInterval: 0,
+      timeDescription: "0 seconds",
     },
   },
   "Binder": {
     acceptableNumbers: {
       perSingleTransaction: 5,
       perTimeInterval: 5,
-      timeInterval: 300000, // 5 minutes
+      timeInterval: 300000,
+      timeDescription: "5 minutes",
     },
   },
   "Blood Kit": {
     acceptableNumbers: {
       perSingleTransaction: 2,
       perTimeInterval: 2,
-      timeInterval: 300000, // 5 minutes
+      timeInterval: 300000,
+      timeDescription: "5 minutes",
     },
   },
   "Car Battery": {
@@ -39,6 +44,7 @@ const ITEM_DEFINITIONS = {
       perSingleTransaction: 0,
       perTimeInterval: 0,
       timeInterval: 0,
+      timeDescription: "0 seconds",
     },
   },
   "Cash Envelope": {
@@ -46,6 +52,7 @@ const ITEM_DEFINITIONS = {
       perSingleTransaction: 0,
       perTimeInterval: 0,
       timeInterval: 0,
+      timeDescription: "0 seconds",
     },
   },
   "Catalytic Converter": {
@@ -53,6 +60,7 @@ const ITEM_DEFINITIONS = {
       perSingleTransaction: 0,
       perTimeInterval: 0,
       timeInterval: 0,
+      timeDescription: "0 seconds",
     },
   },
   "Checkbook": {
@@ -60,69 +68,79 @@ const ITEM_DEFINITIONS = {
       perSingleTransaction: 0,
       perTimeInterval: 0,
       timeInterval: 0,
+      timeDescription: "0 seconds",
     },
   },
   "Cleaning Kit": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 3600000, // 1 hour
+      timeInterval: 3600000,
+      timeDescription: "1 hour",
     },
   },
   "Duffel Bag": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 1814400000, // 3 weeks
+      timeInterval: 1814400000,
+      timeDescription: "3 weeks",
     },
   },
   "EMS Cap": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 1814400000, // 3 weeks
+      timeInterval: 1814400000,
+      timeDescription: "3 weeks",
     },
   },
   "Evidence Box": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 300000, // 5 minutes
+      timeInterval: 300000,
+      timeDescription: "5 minutes",
     },
   },
   "Fingerprint Kit": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 300000, // 5 minutes
+      timeInterval: 300000,
+      timeDescription: "5 minutes",
     },
   },
   "Fire Extinguisher": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 300000, // 5 minutes
+      timeInterval: 300000,
+      timeDescription: "5 minutes",
     },
   },
   "Flare": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 300000, // 5 minutes
+      timeInterval: 300000,
+      timeDescription: "5 minutes",
     },
   },
   "Flashlight": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 1814400000, // 3 weeks
+      timeInterval: 1814400000,
+      timeDescription: "3 weeks",
     },
   },
   "Knife": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 1814400000, // 3 weeks
+      timeInterval: 1814400000,
+      timeDescription: "3 weeks",
     },
   },
   "Gas Can": {
@@ -130,62 +148,71 @@ const ITEM_DEFINITIONS = {
       perSingleTransaction: 0,
       perTimeInterval: 0,
       timeInterval: 0,
+      timeDescription: "0 seconds",
     },
   },
   "GPS Watch": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 1814400000, // 3 weeks
+      timeInterval: 1814400000,
+      timeDescription: "3 weeks",
     },
   },
   "Gov Repair Kit": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 2,
-      timeInterval: 14400000, // 4 hours
+      timeInterval: 14400000,
+      timeDescription: "4 hours",
     },
   },
   "Gov Tyre Mobility Kit": {
     acceptableNumbers: {
       perSingleTransaction: 4,
       perTimeInterval: 4, // if they need more than 4 per 4 hours that means they blew up their ambo twice. wtf
-      timeInterval: 14400000, // 4 hours
+      timeInterval: 14400000,
+      timeDescription: "4 hours",
     },
   },
   "Generic Drug A": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 300000, // 5 minutes
+      timeInterval: 300000,
+      timeDescription: "5 minutes",
     },
   },
   "Generic Drug B": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 300000, // 5 minutes
+      timeInterval: 300000,
+      timeDescription: "5 minutes",
     },
   },
   "Generic Drug C": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 300000, // 5 minutes
+      timeInterval: 300000,
+      timeDescription: "5 minutes",
     },
   },
   "Generic Drug D": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 300000, // 5 minutes
+      timeInterval: 300000,
+      timeDescription: "5 minutes",
     },
   },
   "MDT": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 1814400000, // 3 weeks
+      timeInterval: 1814400000,
+      timeDescription: "3 weeks",
     },
   },
   "Motorcycle Battery": {
@@ -193,27 +220,31 @@ const ITEM_DEFINITIONS = {
       perSingleTransaction: 0,
       perTimeInterval: 0,
       timeInterval: 0,
+      timeDescription: "0 seconds",
     },
   },
   "Pack of Cigarettes": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 14400000, // 4 hours
+      timeInterval: 14400000,
+      timeDescription: "4 hours",
     },
   },
   "Pack of Cigars": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 14400000, // 4 hours
+      timeInterval: 14400000,
+      timeDescription: "4 hours",
     },
   },
   "Paper": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 300000, // 5 minutes
+      timeInterval: 300000,
+      timeDescription: "5 minutes",
     },
   },
   "Prepaid Fuel Card": {
@@ -221,90 +252,103 @@ const ITEM_DEFINITIONS = {
       perSingleTransaction: 0,
       perTimeInterval: 0,
       timeInterval: 0,
+      timeDescription: "0 seconds",
     },
   },
   "Radio": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 1814400000, // 3 weeks
+      timeInterval: 1814400000,
+      timeDescription: "3 weeks",
     },
   },
   "Radio Earpiece": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 1814400000, // 3 weeks
+      timeInterval: 1814400000,
+      timeDescription: "3 weeks",
     },
   },
   "Repair": {
     acceptableNumbers: {
-      perSingleTransaction: 3,
-      perTimeInterval: 3,
-      timeInterval: 300000, // 5 minutes
+      perSingleTransaction: 4,
+      perTimeInterval: 4,
+      timeInterval: 300000,
+      timeDescription: "5 minutes",
     },
   },
   "SAMS Belt": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 1814400000, // 3 weeks
+      timeInterval: 1814400000,
+      timeDescription: "3 weeks",
     },
   },
   "SAMS Boots": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 1814400000, // 3 weeks
+      timeInterval: 1814400000,
+      timeDescription: "3 weeks",
     },
   },
   "SAMS Holster": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 1814400000, // 3 weeks
+      timeInterval: 1814400000,
+      timeDescription: "3 weeks",
     },
   },
   "SAMS Pants": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 1814400000, // 3 weeks
+      timeInterval: 1814400000,
+      timeDescription: "3 weeks",
     },
   },
   "SAMS Scrub Pants": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 1814400000, // 3 weeks
+      timeInterval: 1814400000,
+      timeDescription: "3 weeks",
     },
   },
   "SAMS Scrub Top": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 1814400000, // 3 weeks
+      timeInterval: 1814400000,
+      timeDescription: "3 weeks",
     },
   },
   "SAMS Skirt": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 1814400000, // 3 weeks
+      timeInterval: 1814400000,
+      timeDescription: "3 weeks",
     },
   },
   "Sample Kit": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 300000, // 5 minutes
+      timeInterval: 300000,
+      timeDescription: "5 minutes",
     },
   },
   "Saliva Swab": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 300000, // 5 minutes
+      timeInterval: 300000,
+      timeDescription: "5 minutes",
     },
   },
   "Seatbelt Set": {
@@ -312,76 +356,87 @@ const ITEM_DEFINITIONS = {
       perSingleTransaction: 0,
       perTimeInterval: 0,
       timeInterval: 0,
+      timeDescription: "0 seconds",
     },
   },
   "Stethoscope": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 1814400000, // 3 weeks
+      timeInterval: 1814400000,
+      timeDescription: "3 weeks",
     },
   },
   "TacMed Balaclava": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 1814400000, // 3 weeks
+      timeInterval: 1814400000,
+      timeDescription: "3 weeks",
     },
   },
   "TacMed Cargo Pants": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 1814400000, // 3 weeks
+      timeInterval: 1814400000,
+      timeDescription: "3 weeks",
     },
   },
   "TacMed First Responder Jacket": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 1814400000, // 3 weeks
+      timeInterval: 1814400000,
+      timeDescription: "3 weeks",
     },
   },
   "TacMed Gaiter": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 1814400000, // 3 weeks
+      timeInterval: 1814400000,
+      timeDescription: "3 weeks",
     },
   },
   "TacMed Pants": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 1814400000, // 3 weeks
+      timeInterval: 1814400000,
+      timeDescription: "3 weeks",
     },
   },
   "TacMed T-Shirt": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 1814400000, // 3 weeks
+      timeInterval: 1814400000,
+      timeDescription: "3 weeks",
     },
   },
   "TecMed Headset": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 1814400000, // 3 weeks
+      timeInterval: 1814400000,
+      timeDescription: "3 weeks",
     },
   },
   "TecMed Vest": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 1814400000, // 3 weeks
+      timeInterval: 1814400000,
+      timeDescription: "3 weeks",
     },
   },
   "Tactical Flashlight": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 1814400000, // 3 weeks
+      timeInterval: 1814400000,
+      timeDescription: "3 weeks",
     },
   },
   "Traffic Cone": {
@@ -389,13 +444,15 @@ const ITEM_DEFINITIONS = {
       perSingleTransaction: 0,
       perTimeInterval: 0,
       timeInterval: 0,
+      timeDescription: "0 seconds",
     },
   },
   "Wheelchair": {
     acceptableNumbers: {
       perSingleTransaction: 1,
       perTimeInterval: 1,
-      timeInterval: 300000, // 5 minutes
+      timeInterval: 300000,
+      timeDescription: "5 minutes",
     },
   },
 };
