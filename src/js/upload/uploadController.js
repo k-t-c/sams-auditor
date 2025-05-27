6cd44b46df043ef3c04b681b6e6f013b4a64728a
KC <nachosktc@gmail.com>
2025-05-27 13:17:04 -0600

    formating...

--- src/js/upload/uploadController.js
+++ src/js/upload/uploadController.js
@@ -24,21 +24,21 @@ function uploadDataReady() {
   reportViolations();
 }
   
-  document.getElementById('csvUpload').addEventListener('change', function (e) {
-    const file = e.target.files[0];
-    if (!file) return;
-    const noticeDiv = document.getElementById("uploadViewNotice");
-    if (noticeDiv) {
-      noticeDiv.innerHTML = "<p>Processing...</p>"
-    }
-    
-    uploadProcessCSV(file);
+document.getElementById('csvUpload').addEventListener('change', function (e) {
+  const file = e.target.files[0];
+  if (!file) return;
+  const noticeDiv = document.getElementById("uploadViewNotice");
+  if (noticeDiv) {
+    noticeDiv.innerHTML = "<p>Processing...</p>"
+  }
   
-    // reattach onchange listener to dropdown in case user wants to refilter
-    document.getElementById('arsenalItemSelector').onchange = () => { 
-      arsenalProcessSelection();
-    };
-    document.getElementById("arsenalChartType").addEventListener ("change", function(e) {
-      arsenalProcessSelection();
-    });
+  uploadProcessCSV(file);
+
+  // reattach onchange listener to dropdown in case user wants to refilter
+  document.getElementById('arsenalItemSelector').onchange = () => { 
+    arsenalProcessSelection();
+  };
+  document.getElementById("arsenalChartType").addEventListener ("change", function(e) {
+    arsenalProcessSelection();
   });
+});
