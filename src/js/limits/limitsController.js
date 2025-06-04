fa79c5dd15e694e7194426d94696d17967c0a497
KC <nachosktc@gmail.com>
2025-06-04 16:21:38 -0600

    create limitsHandleChange handler function

--- src/js/limits/limitsController.js
+++ src/js/limits/limitsController.js
@@ -10,33 +10,7 @@ function limitsDataReady() {
 
   const itemLimitsForm = document.querySelector("#itemLimitsView");
   itemLimitsForm.addEventListener("change", (event) => {
-    const target = event.target;
-    const id = target.id;
-    const itemName = target.parentElement.id.slice(6).replace("-", " ");
-
-    // const currentValue
-    switch (true) {
-      case id.includes("-singlePurchaseInput"):
-        limitsSetSingleTransaction(itemName, parseInt(target.value));
-        break;
-      case id.includes("-perTimeIntervalInput"):
-        limitsSetPerTimeInterval(itemName, parseInt(target.value));
-        break;
-      case id.includes("-timeIntervalInput") || id.includes("-timeIntervalSelect"):
-        const inputId = `limit-${itemName.replace(" ", "-")}-timeIntervalInput`;
-        const selectId = `limit-${itemName.replace(" ", "-")}-timeIntervalSelect`;
-
-        const inputValue = document.getElementById(inputId)?.value;
-        const selectValue = document.getElementById(selectId)?.value;
-        const newLimitStr = `${inputValue} ${selectValue}`;
-        const newLimit = getMsFromString(newLimitStr);
-        limitsSetTimeInterval(itemName, newLimit);
-
-        break;
-      default:
-        // TODO: error handing
-        break;
-    }
+    limitsHandleChange(event);
   });
 }
 
